e7cf3ec1be44e6a17df71225e5391f59fb61ad0c
Fernando Ferreira de Oliveira <bse.fernando@gmail.com>
2014-12-12 15:12:33 -0200

    Count trailing zeroes in factorial

---
+++ factorial_trailing_zeros.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+/* Given an integer n, write a function that returns count of trailing zeroes in n!. */
+
+int findTrailingZeros(int  n) {
+    int cnt = 0;
+    // Keep dividing n by powers of 5 and update count
+    for(int i = 5; n/i >= 1; i *= 5) cnt += n/i;
+    return cnt;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d\n", findTrailingZeros(n));
+    return 0;
+}
