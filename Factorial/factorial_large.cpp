339c5527e8191506069cb92c7175aa37328b4c97
Fernando Ferreira de Oliveira <bse.fernando@gmail.com>
2015-01-15 12:27:26 -0200

    Factorial for large numbers

--- Factorial/factorial_large.cpp
+++ Factorial/factorial_large.cpp
@@ -1,299 +1,362 @@
-#include <bits/stdc++.h>
-#define uint unsigned int
-#define ull unsigned long long
+#include <iostream>
+#include <cstdio>
+#include <iomanip>
+#include <vector>
+
 using namespace std;
+const int base = 1000000000, base_digits = 9;
 
-class MathRoutines {
-    public:
-      template <class Element>
-      static void RaiseToPower(Element& theElement, int thePower, const Element& theRingUnit);
-      inline static int Maximum(int a, int b){  if (a>b) return a; else return b; }
-};
+/*  Factorial for large numbers */
+struct bigint {
+    vector<int> a;
+    int sign;
 
-class LargeUI {
-    vector<unsigned int> theDigits;
-    void AddNoFitSize(const LargeUI& x);
-    public:
-        //The zero element is assumed to have length one array with a zero entry.
-        //CarryOverBound is the "base" over which we work.
-        //Requirements on the CarryOverBound:
-        //1.  CarryOverBound*2-1 must fit inside an unsigned (int)
-        //    on the system
-        //2. (CarryOverBound*2)^2-1 must fit inside (long long)
-        //    on the system.
-        static const unsigned int CarryOverBound = 1000000000UL;
-        void SubtractSmallerPositive(const LargeUI& x);
-        inline int size()const{return this->theDigits.size();}
-        string GetFactorial()const;
-        void MakeOne(){ this->theDigits.resize(1);  this->theDigits[0] = 1;}
-        void MakeZero(){ this->theDigits.resize(1); this->theDigits[0] = 0;}
-        bool IsGEQ(const LargeUI& x)const;
+    bigint() :
+        sign(1) {
+    }
 
-        /* Prime numbers using Sieve of Erathostenes */
-        static void GetPrimes(unsigned int n, std::vector<unsigned int>& output) {
-            vector<int> primes;
-            primes.resize(n+1);
+    bigint(long long v) {
+        *this = v;
+    }
 
-            for(uint i = 0; i < n+1; i++) primes[i] = 1;
-            output.resize(0);
-            output.reserve(n/2);
+    bigint(const string &s) {
+        read(s);
+    }
 
-            for(uint i = 2; i <= n; i++)
-                if(primes[i] != 0) {
-                    output.push_back(i);
-                    for (uint j = i; j <= n; j += i) primes[j] = 0;
+    void operator = (long long v) {
+        sign = 1;
+        if(v < 0) sign = -1, v = -v;
+        for(; v > 0; v = v / base) a.push_back(v % base);
+    }
+
+    bigint operator + (const bigint &v) const {
+        if(sign == v.sign) {
+            bigint res = v;
+
+            for(int i = 0, carry = 0; i < (int) max(a.size(), v.a.size()) || carry; ++i) {
+                if(i == (int) res.a.size()) res.a.push_back(0);
+
+                res.a[i] += carry + (i < (int) a.size() ? a[i] : 0);
+                carry = res.a[i] >= base;
+
+                if(carry) res.a[i] -= base;
+            }
+            return res;
+        }
+        return *this - (-v);
+    }
+
+    bigint operator - (const bigint &v) const {
+        if(sign == v.sign) {
+            if (abs() >= v.abs()) {
+                bigint res = *this;
+
+                for(int i = 0, carry = 0; i < (int) v.a.size() || carry; ++i) {
+                    res.a[i] -= carry + (i < (int) v.a.size() ? v.a[i] : 0);
+                    carry = res.a[i] < 0;
+
+                    if(carry) res.a[i] += base;
                 }
+
+                res.trim();
+                return res;
+            }
+            return -(v - *this);
+        }
+        return *this + (-v);
+    }
+
+    void operator *= (int v) {
+        if(v < 0) sign = -sign, v = -v;
+
+        for(int i = 0, carry = 0; i < (int) a.size() || carry; ++i) {
+            if(i == (int) a.size()) a.push_back(0);
+
+            long long cur = a[i] * (long long) v + carry;
+            carry = (int) (cur / base);
+            a[i] = (int) (cur % base);
+        }
+        trim();
+    }
+
+    bigint operator * (int v) const {
+        bigint res = *this;
+        res *= v;
+        return res;
+    }
+
+    bigint operator / (const bigint &v) const {
+        return divmod(*this, v).first;
+    }
+
+    bigint operator % (const bigint &v) const {
+        return divmod(*this, v).second;
+    }
+
+    friend pair<bigint, bigint> divmod(const bigint &a1, const bigint &b1) {
+        int norm = base / (b1.a.back() + 1);
+        bigint a = a1.abs() * norm;
+        bigint b = b1.abs() * norm;
+        bigint q, r;
+        q.a.resize(a.a.size());
+
+        for(int i = a.a.size() - 1; i >= 0; i--) {
+            r *= base;
+            r += a.a[i];
+
+            int s1 = r.a.size() <= b.a.size() ? 0 : r.a[b.a.size()];
+            int s2 = r.a.size() <= b.a.size() - 1 ? 0 : r.a[b.a.size() - 1];
+            int d = ((long long) base * s1 + s2) / b.a.back();
+            r -= b * d;
+
+            while(r < 0) r += b, --d;
+            q.a[i] = d;
         }
 
-        inline void operator *= (const LargeUI& right);
-        void AssignFactorial(unsigned int x);
-        void MultiplyBy(const LargeUI& x, LargeUI& output)const;
-        void AddInt(unsigned int x, int shift); /* Add shifted int smaller than carry over bound */
-        void AssignShiftedUInt(unsigned int x, int shift);
-        inline void operator += (const LargeUI& other);
+        q.sign = a1.sign * b1.sign;
+        r.sign = a1.sign;
+        q.trim();
+        r.trim();
+        return make_pair(q, r / norm);
+    }
 
-        inline void operator = (const LargeUI& other) {
-            this->theDigits = other.theDigits;
+    void operator /= (int v) {
+        if (v < 0) sign = -sign, v = -v;
+
+        for(int i = (int) a.size() - 1, rem = 0; i >= 0; --i) {
+            long long cur = a[i] + rem * (long long) base;
+            a[i] = (int) (cur / v);
+            rem = (int) (cur % v);
+        }
+        trim();
+    }
+
+    bigint operator / (int v) const {
+        bigint res = *this;
+        res /= v;
+        return res;
+    }
+
+    int operator % (int v) const {
+        if(v < 0) v = -v;
+        int m = 0;
+
+        for(int i = a.size() - 1; i >= 0; --i)
+            m = (a[i] + m * (long long) base) % v;
+
+        return m * sign;
+    }
+
+    void operator += (const bigint &v) {
+        *this = *this + v;
+    }
+    void operator -= (const bigint &v) {
+        *this = *this - v;
+    }
+    void operator *= (const bigint &v) {
+        *this = *this * v;
+    }
+    void operator /= (const bigint &v) {
+        *this = *this / v;
+    }
+
+    bool operator < (const bigint &v) const {
+        if(sign != v.sign) return sign < v.sign;
+
+        if(a.size() != v.a.size())
+            return a.size() * sign < v.a.size() * v.sign;
+
+        for(int i = a.size() - 1; i >= 0; i--)
+            if(a[i] != v.a[i]) return a[i] * sign < v.a[i] * sign;
+        return false;
+    }
+
+    bool operator>(const bigint &v) const {
+        return v < *this;
+    }
+    bool operator<=(const bigint &v) const {
+        return !(v < *this);
+    }
+    bool operator>=(const bigint &v) const {
+        return !(*this < v);
+    }
+    bool operator==(const bigint &v) const {
+        return !(*this < v) && !(v < *this);
+    }
+    bool operator!=(const bigint &v) const {
+        return *this < v || v < *this;
+    }
+
+    void trim() {
+        while (!a.empty() && !a.back()) a.pop_back();
+        if(a.empty()) sign = 1;
+    }
+
+    bool isZero() const {
+        return a.empty() || (a.size() == 1 && !a[0]);
+    }
+
+    bigint operator-() const {
+        bigint res = *this;
+        res.sign = -sign;
+        return res;
+    }
+
+    bigint abs() const {
+        bigint res = *this;
+        res.sign *= res.sign;
+        return res;
+    }
+
+    long long longValue() const {
+        long long res = 0;
+
+        for(int i = a.size() - 1; i >= 0; i--)
+            res = res * base + a[i];
+        return res * sign;
+    }
+
+    friend bigint gcd(const bigint &a, const bigint &b) {
+        return b.isZero() ? a : gcd(b, a % b);
+    }
+    friend bigint lcm(const bigint &a, const bigint &b) {
+        return a / gcd(a, b) * b;
+    }
+
+    void read(const string &s) {
+        sign = 1;
+        a.clear();
+        int pos = 0;
+
+        while(pos < (int) s.size() && (s[pos] == '-' || s[pos] == '+')) {
+            if(s[pos] == '-') sign = -sign;
+            ++pos;
         }
 
-        LargeUI() {
-            this->theDigits.push_back(0);
+        for(int i = s.size() - 1; i >= pos; i -= base_digits) {
+            int x = 0;
+            for(int j = max(pos, i - base_digits + 1); j <= i; j++)
+                x = x * 10 + s[j] - '0';
+            a.push_back(x);
+        }
+        trim();
+    }
+
+    friend istream& operator>>(istream &stream, bigint &v) {
+        string s;
+        stream >> s;
+        v.read(s);
+        return stream;
+    }
+
+    friend ostream& operator<<(ostream &stream, const bigint &v) {
+        if(v.sign == -1) stream << '-';
+        stream << (v.a.empty() ? 0 : v.a.back());
+
+        for(int i = (int) v.a.size() - 2; i >= 0; --i)
+            stream << setw(base_digits) << setfill('0') << v.a[i];
+        return stream;
+    }
+
+    static vector<int> convert_base(const vector<int> &a, int old_digits, int new_digits) {
+        vector<long long> p(max(old_digits, new_digits) + 1);
+        p[0] = 1;
+
+        for(int i = 1; i < (int) p.size(); i++)
+            p[i] = p[i - 1] * 10;
+
+        vector<int> res;
+        long long cur = 0;
+        int cur_digits = 0;
+
+        for(int i = 0; i < (int) a.size(); i++) {
+            cur += a[i] * p[cur_digits];
+            cur_digits += old_digits;
+
+            while(cur_digits >= new_digits) {
+                res.push_back(int(cur % p[new_digits]));
+                cur /= p[new_digits];
+                cur_digits -= new_digits;
+            }
         }
 
-        void FitSize();
+        res.push_back((int) cur);
+
+        while(!res.empty() && !res.back()) res.pop_back();
+        return res;
+    }
+
+    typedef vector<long long> vll;
+
+    static vll karatsubaMultiply(const vll &a, const vll &b) {
+        int n = a.size();
+        vll res(n + n);
+
+        if(n <= 32) {
+            for(int i = 0; i < n; i++)
+                for(int j = 0; j < n; j++) res[i + j] += a[i] * b[j];
+            return res;
+        }
+
+        int k = n >> 1;
+        vll a1(a.begin(), a.begin() + k);
+        vll a2(a.begin() + k, a.end());
+        vll b1(b.begin(), b.begin() + k);
+        vll b2(b.begin() + k, b.end());
+
+        vll a1b1 = karatsubaMultiply(a1, b1);
+        vll a2b2 = karatsubaMultiply(a2, b2);
+
+        for(int i = 0; i < k; i++) a2[i] += a1[i];
+        for(int i = 0; i < k; i++) b2[i] += b1[i];
+
+        vll r = karatsubaMultiply(a2, b2);
+
+        for(int i = 0; i < (int) a1b1.size(); i++) r[i] -= a1b1[i];
+        for(int i = 0; i < (int) a2b2.size(); i++) r[i] -= a2b2[i];
+
+        for(int i = 0; i < (int) r.size(); i++) res[i + k] += r[i];
+
+        for(int i = 0; i < (int) a1b1.size(); i++) res[i] += a1b1[i];
+        for(int i = 0; i < (int) a2b2.size(); i++) res[i + n] += a2b2[i];
+        return res;
+    }
+
+    bigint operator*(const bigint &v) const {
+        vector<int> a6 = convert_base(this->a, base_digits, 6);
+        vector<int> b6 = convert_base(v.a, base_digits, 6);
+        vll a(a6.begin(), a6.end());
+        vll b(b6.begin(), b6.end());
+
+        while(a.size() < b.size()) a.push_back(0);
+        while(b.size() < a.size()) b.push_back(0);
+        while(a.size() & (a.size() - 1)) a.push_back(0), b.push_back(0);
+
+        vll c = karatsubaMultiply(a, b);
+        bigint res;
+        res.sign = sign * v.sign;
+
+        for(int i = 0, carry = 0; i < (int) c.size(); i++) {
+            long long cur = c[i] + carry;
+            res.a.push_back((int) (cur % 1000000));
+            carry = (int) (cur / 1000000);
+        }
+
+        res.a = convert_base(res.a, 6, base_digits);
+        res.trim();
+        return res;
+    }
 };
 
-void LargeUI::AssignFactorial(unsigned int x) {
-    this->MakeOne();
-    vector<unsigned int> primesBelowX;
-    LargeUI::GetPrimes(x, primesBelowX);
-    LargeUI tempInt, tempOne;
-
-    tempOne.MakeOne();
-
-    for(uint i = 0; i < primesBelowX.size(); i++) {
-        uint thePrime = primesBelowX[i];
-        uint thePowerOfThePrime = 0;
-        uint currentPower = thePrime;
-
-        do {
-            thePowerOfThePrime += x/currentPower;
-            currentPower *= thePrime;
-        } while (currentPower <= x);
-
-        tempInt.AssignShiftedUInt(thePrime, 0);
-        MathRoutines::RaiseToPower(tempInt, thePowerOfThePrime, tempOne);
-        *this *= tempInt;
-    }
-}
-
-template <class Element>
-void MathRoutines::RaiseToPower(Element& theElement, int thePower, const Element& theOne) {
-    if(thePower <= 1) return;
-
-    if(thePower == 0) {
-        theElement = theOne;
-        return;
-    }
-
-    Element Result = theOne;
-
-    if(thePower < 4) {
-        for(int i = 0; i < thePower; i++) Result.operator *= (theElement);
-        theElement = Result;
-        return;
-    }
-
-    vector<Element> containerList;
-    int log2RoundedDown = 0;
-    int highest = 1; /* Highest power, lower than or equal to the power*/
-
-    for(; highest <= thePower; highest *= 2) log2RoundedDown++;
-
-    highest /= 2;
-    log2RoundedDown--;
-    containerList.reserve(log2RoundedDown);
-    Result = theElement;
-    containerList.push_back(Result);
-
-    for(int i = 1; i <= log2RoundedDown; i++) {
-        Result.operator *= (Result);
-        containerList.push_back(Result);
-    }
-
-    thePower -= highest;
-    highest /= 2;
-    int currentIndex = containerList.size()-2;
-
-    for(; thePower > 0;) {
-        if(thePower >= highest){
-            Result.operator *= (containerList[currentIndex]);
-            thePower -= highest;
-        }
-
-        currentIndex--;
-        highest /= 2;
-    }
-
-    theElement = Result;
-}
-
-inline void LargeUI::AddInt(uint x, int shift) {
-    while(x > 0){
-        if(shift >= this->size()) {
-            int oldsize = this->size();
-            this->theDigits.resize(shift+1);
-
-            for(int i = oldsize; i < this->size(); i++) this->theDigits[i] = 0;
-        }
-
-        this->theDigits[shift] += x;
-
-        if(this->theDigits[shift] >= LargeUI::CarryOverBound) {
-            this->theDigits[shift] -= LargeUI::CarryOverBound;
-            x=1;
-            shift++;
-        } else x = 0;
-    }
-}
-
-inline void LargeUI::AssignShiftedUInt(uint x, int shift) {
-    if (x == 0) {
-        this->MakeZero();
-        return;
-    }
-
-    this->theDigits.resize(shift+1);
-
-    for(int i = 0; i < shift; i++) this->theDigits[i] = 0;
-
-    uint tempX = x % LargeUI::CarryOverBound;
-    this->theDigits[shift]=tempX;
-    x = x/LargeUI::CarryOverBound;
-
-    while(x != 0) {
-        tempX = x % LargeUI::CarryOverBound;
-        this->theDigits.push_back(tempX);
-        x = x/LargeUI::CarryOverBound;
-    }
-}
-
-inline void LargeUI::AddNoFitSize(const LargeUI& x) {
-    int oldsize= this->size();
-    this->theDigits.resize(MathRoutines::Maximum(this->size(), x.size())+1);
-
-    for(int i = oldsize; i < this->size(); i++) this->theDigits[i] = 0;
-
-    uint CarryOver = 0;
-    for(int i = 0; i < x.size(); i++) {
-        this->theDigits[i] += x.theDigits[i] + CarryOver;
-
-        if(this->theDigits[i] >= LargeUI::CarryOverBound) {
-            this->theDigits[i] -= LargeUI::CarryOverBound;
-            CarryOver = 1;
-        } else CarryOver = 0;
-    }
-
-    if (CarryOver != 0)
-        for(int i = x.size(); i < this->size(); i++) {
-            this->theDigits[i] += 1;
-            if (this->theDigits[i] >= LargeUI::CarryOverBound)
-                this->theDigits[i] -= LargeUI::CarryOverBound;
-            else break;
-        }
-}
-
-void LargeUI::operator += (const LargeUI& x) {
-    this->AddNoFitSize(x);
-    this->FitSize();
-}
-
-void LargeUI::SubtractSmallerPositive(const LargeUI& x) {
-    uint CarryOver = 0;
-    for(int i = 0; i < x.size(); i++)
-        if(this->theDigits[i] < x.theDigits[i] + CarryOver) {
-            this->theDigits[i] += LargeUI::CarryOverBound;
-            this->theDigits[i] -= (x.theDigits[i] + CarryOver);
-            CarryOver = 1;
-        } else {
-            this->theDigits[i] -= (x.theDigits[i] + CarryOver);
-            CarryOver = 0;
-        }
-
-    if(CarryOver != 0) {
-        for(int i = x.size(); i < this->size(); i++)
-            if(this->theDigits[i] > 0) {
-                this->theDigits[i]--;
-                break;
-            } else this->theDigits[i] = LargeUI::CarryOverBound-1;
-    }
-
-    this->FitSize();
-}
-
-void LargeUI::MultiplyBy(const LargeUI& x, LargeUI& output) const {
-    output.theDigits.resize(x.size() + output.size());
-
-    for(int i = 0; i < output.size(); i++) output.theDigits[i] = 0;
-
-    for(int i = 0; i < this->size(); i++)
-        for(int j = 0; j < x.size(); j++) {
-            ull tempLong = this->theDigits[i];
-            ull tempLong2 = x.theDigits[j];
-            tempLong = tempLong*tempLong2;
-            ull lowPart = tempLong % LargeUI::CarryOverBound;
-            ull highPart = tempLong/LargeUI::CarryOverBound;
-            output.AddInt((uint) lowPart, i+j);
-            output.AddInt((uint) highPart, i+j+1);
-        }
-
-    output.FitSize();
-}
-
-void LargeUI::FitSize() {
-    int newSize = this->size();
-
-    for(int i = this->size()-1; i >= 1; i--)
-        if(this->theDigits[i] == 0) newSize--;
-        else break;
-    this->theDigits.resize(newSize);
-}
-
-void LargeUI::operator *= (const LargeUI& x) {
-    LargeUI tempInt;
-    this->MultiplyBy(x, tempInt);
-    this->operator = (tempInt);
-}
-
-
-
-string LargeUI::GetFactorial() const {
-    stringstream out;
-
-    if(this->CarryOverBound != 1000000000UL) return "This method is not implemented";
-
-    string tempS;
-    out << (this->theDigits[this->size()-1]);
-
-    for(int i = this->size()-2; i >= 0; i--) {
-        stringstream tempStream;
-        tempStream << this->theDigits[i];
-        tempS = tempStream.str();
-        int numZeroesToPad = 9 - tempS.length();
-        for(int j = 0; j < numZeroesToPad; j++) out << "0";
-        out << tempS;
-    }
-
-    return out.str();
+void getFactorial(int t) {
+    bigint fact = 1;
+    for(int i = 1; i <= t; i++) fact *= i;
+    cout << fact << endl;
 }
 
 int main() {
-    LargeUI tempInt;
-    uint x;
-
-    scanf("%u", &x);
-
-    tempInt.AssignFactorial(x);
-    printf("%s\n", tempInt.GetFactorial().c_str());
+    int t;
+    scanf("%d", &t);
+    getFactorial(t);
     return 0;
 }
